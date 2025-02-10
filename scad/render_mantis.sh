b44cb1f069d76baa9aa182b27f1498e7e0b1fac9
Felix Kuehling <felix.kuehling@gmail.com>
2025-02-10 15:17:03 -0500

    Update render-mantis for openscad-nightly
    Use openscad-nightly with Manifold backend if available. This speeds up
    rendering from minutes to seconds and reduces memory usage.
    
    Signed-off-by: Felix Kuehling <felix.kuehling@gmail.com>

--- scad/render_mantis.sh
+++ scad/render_mantis.sh
@@ -5,6 +5,13 @@ cd ${0%/*}
 
 version=1.0
 scad="mantis-v$version.scad"
+osc=`which openscad-nightly`
+if [ ! -x "$osc" ]; then
+	osc="openscad"
+	osc_params=""
+else
+	osc_params="--backend=Manifold"
+fi
 
 # Supersampling
 size=$((1920*3))
@@ -14,44 +21,44 @@ sizeL=$size,$((size*3/4))
 sizeP=$((size*3/4)),$size
 
 # Sizes to scale down to
-sizes="1920 960 640"
+sizes="1920 1440 1152"
 
 images="mantis.png mantis_bare.png mantis_naked.png mantis_top.png mantis_bottom.png mantis_exploded.png"
 
-params="--colorscheme=DeepOcean -D \$fs=0.5 -D \$fa=1 -D render_pcbs=true -D bottom_color=\"purple\" -D top_color=\"white\""
+params="$osc_params --colorscheme=DeepOcean -D \$fs=0.5 -D \$fa=1 -D render_pcbs=true -D bottom_color=\"purple\" -D top_color=\"white\""
 
 trap 'echo "Killing background processes ..."; kill $jobs' INT
 
 echo "Perspective view ..."
-openscad $params --camera=15,12,10,50,0,22,450 --imgsize=$sizeW \
+$osc $params --camera=15,12,10,50,0,22,450 --imgsize=$sizeW \
 	-o "$dir/mantis.png" $scad &
 jobs="$jobs $!"
 
 echo "Bare-bones view ..."
-openscad $params --camera=15,12,10,50,0,22,450 --imgsize=$sizeW \
+$osc $params --camera=15,12,10,50,0,22,450 --imgsize=$sizeW \
 	-D show_trackball=false -D show_key=false \
 	-o "$dir/mantis_bare.png" $scad &
 jobs="$jobs $!"
 
 echo "Naked view ..."
-openscad $params --camera=15,12,10,50,0,22,450 --imgsize=$sizeW \
+$osc $params --camera=15,12,10,50,0,22,450 --imgsize=$sizeW \
 	-D show_trackball=false -D show_key=false -D show_switch=false \
 	-D case_alpha=0.4 -o "$dir/mantis_naked.png" $scad &
 jobs="$jobs $!"
 
 echo "Top-down view ..."
-openscad $params --camera=5,25,0,0,0,30,500 --imgsize=$sizeL \
+$osc $params --camera=5,25,0,0,0,30,500 --imgsize=$sizeL \
 	--projection=ortho -o "$dir/mantis_top.png" $scad &
 jobs="$jobs $!"
 
 echo "Bottom-up view ..."
-openscad $params --camera=0,8,0,180,0,180,400 --imgsize=$sizeW \
+$osc $params --camera=0,8,0,180,0,180,400 --imgsize=$sizeW \
 	-D case_alpha=0.2 -D show_desk=false --projection=ortho \
 	-o "$dir/mantis_bottom.png" $scad &
 jobs="$jobs $!"
 
 echo "Exploded view ..."
-openscad $params --camera=5,30,270,65,0,15,1300 --imgsize=$sizeP \
+$osc $params --camera=5,30,270,65,0,15,1300 --imgsize=$sizeP \
 	-D \$explode=60 --projection=ortho \
 	-o "$dir/mantis_exploded.png" $scad &
 jobs="$jobs $!"
@@ -72,7 +79,7 @@ done
 for img in $images; do
 	for size in $sizes; do
 		echo "Downscaling $img to $size"
-		convert "$dir/$img" -colorspace RGB -gaussian-blur 0x1 -attenuate 0.1 +noise Gaussian -resize ${size}x${size} -sampling-factor 4:2:0 -strip -quality 95 -interlace JPEG "$dir/$size/v$version-${img%.png}.jpg" &
+		convert "$dir/$img" -colorspace RGB -gaussian-blur 0x1 -attenuate 0.1 +noise Gaussian -resize ${size}x${size} -sampling-factor 4:2:0 -colorspace sRGB -strip -quality 95 -interlace JPEG "$dir/$size/v$version-${img%.png}.jpg" &
 		jobs="$jobs $!"
 	done
 done
