fxkuehl/mantis

commit 9a96d308be9cc980ad6be8dcd6c033ee6fdaaf0a

Chamfer rear mez gasket pads for easier install
Chamfer rear gasket pads at a 12° angle for easier installation. This
makes part of the pad unusable for the gasket. However, there are 8mm of
usable space left for the gasket.

Signed-off-by: Felix Kuehling <felix.kuehling@gmail.com>

scad/mantis-v1.0.scad
@@ -650,13 +650,22 @@ module sensor() {
 
 module raised_gasket_pads()
     translate([0, 0, main_height - deck_thickness + base_thickness - vfit])
-        linear_extrude(gasket_pad_thickness, convexity=10)
-        offset(r = f_key + s_key) offset(delta = -f_key - s_key - s_pcb)
-        union() {
-        translate([-2*hx -  dx/2, -2*hy - dy]) hex_outline();
-        translate([ 2*hx +  dx/2, -2*hy - dy]) hex_outline();
-        translate([0, 3*hy + hy/6]) square([2*hx + dx, hy], center=true);
-    }
+        intersection() {
+            linear_extrude(gasket_pad_thickness, convexity=10)
+                offset(r = f_key + s_key) offset(delta = -f_key - s_key - s_pcb)
+                union() {
+                    translate([-2*hx -  dx/2, -2*hy - dy]) hex_outline();
+                    translate([ 2*hx +  dx/2, -2*hy - dy]) hex_outline();
+                    translate([0, 3*hy + hy/6])
+                        square([2*hx + dx, hy], center=true);
+                }
+            pivot = [0, mcu_top - post_diameter, -deck_thickness];
+            radius = deck_thickness + gasket_pad_thickness;
+            angle = 12;
+            translate(pivot) rotate([0, 90, 0])
+                linear_extrude(3*hx, center=true) offset(r = radius)
+                polygon([[0, -hy], [0, 0], hy*[sin(angle), cos(angle)]]);
+        }
 module mezzanine() {
     ca = render_case ? 0 : case_alpha;
     difference() {

commit 2f8824c20d22370ae1c92386a4a0184ee2d8776d

Fix missing gasket pads on Mezzanine
They were cut off accidentally when I added chamfer to the rear gasket
pads.

Fixes: 9a96d308be9c ("Chamfer rear mez gasket pads for easier install")
Signed-off-by: Felix Kuehling <felix.kuehling@gmail.com>

scad/mantis-v1.0.scad
@@ -789,8 +789,8 @@ module raised_gasket_pads()
             radius = deck_thickness + gasket_pad_thickness;
             angle = 12;
             translate(pivot) rotate([0, 90, 0])
-                linear_extrude(3*hx, center=true) offset(r = radius)
-                polygon([[0, -hy], [0, 0], hy*[sin(angle), cos(angle)]]);
+                linear_extrude(6*hx, center=true) offset(r = radius)
+                polygon([[0, -6*hy], [0, 0], hy*[sin(angle), cos(angle)]]);
         }
 module mezzanine() {
     ca = render_case ? 0 : case_alpha;